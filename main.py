e9aa066a0eeb458000731635aeb06d71bc30e0b4
kisamesharingan-sketch <kisame.sharingan@gmail.com>
2026-05-14 03:04:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ async def on_ready():
     members = load_members()
     for guild in client.guilds:
         for member in guild.members:
-            if not member.bot:
+            if True  # include bots:
                 members[member.name.lower()] = member.id
                 members[member.display_name.lower()] = member.id
     save_members(members)
